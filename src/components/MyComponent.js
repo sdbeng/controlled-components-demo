b92f43537d43f90f60da62d13543b353d36c4c3c
Sergio Barrera <sdbeng@gmail.com>
2019-02-10 12:35:54 -0800

    Clean up comments

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,40 +1,40 @@
 import React from 'react'
 class MyComponent extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        visibility: false
-      };
-      // change code below this line
-  this.toggleVisibility = this.toggleVisibility.bind(this)
-      // change code above this line
+        super(props);
+        this.state = {
+            visibility: false
+        };
+
+        this.toggleVisibility = this.toggleVisibility.bind(this)
+
     }
-    // change code below this line
-    toggleVisibility(){
+
+    toggleVisibility() {
         this.setState({
-          visibility:!this.state.visibility
+            visibility: !this.state.visibility
         })
         console.log('LOG STATE:', this.state.visibility)
-        }
-    // change code above this line
-    render() {
-      if (this.state.visibility) {
-        return (
-          <div>
-            <button 
-            className="btn btn-primary"
-            onClick={this.toggleVisibility}>Click Me</button>
-            <h1>Now you see me!</h1>
-          </div>
-        );
-      } else {
-        return (
-          <div className="container">
-          <h2>Visibility toggle</h2>
-            <button onClick={this.toggleVisibility}>Click Me</button>
-          </div>
-        );
-      }
     }
-  };
-  export default MyComponent
+
+    render() {
+        if (this.state.visibility) {
+            return (
+                <div>
+                    <button
+                        className="btn btn-primary"
+                        onClick={this.toggleVisibility}>Click Me</button>
+                    <h1>Now you see me!</h1>
+                </div>
+            );
+        } else {
+            return (
+                <div className="container">
+                    <h2>Visibility toggle</h2>
+                    <button onClick={this.toggleVisibility}>Click Me</button>
+                </div>
+            );
+        }
+    }
+};
+export default MyComponent
