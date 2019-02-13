99117a56651fdf3a29569aa07e73cc206f2f5b0f
Sergio Barrera <sdbeng@gmail.com>
2019-02-12 21:35:43 -0800

    works

--- src/components/AdvancedJS.js
+++ src/components/AdvancedJS.js
@@ -1,13 +1,80 @@
+/* React: Use Advanced JavaScript in React Render Method
+In previous challenges, you learned how to inject JavaScript code into JSX code using curly braces, { }, for tasks like accessing props, passing props, accessing state, inserting comments into your code, and most recently, styling your components. These are all common use cases to put JavaScript in JSX, but they aren't the only way that you can utilize JavaScript code in your React components.
+
+You can also write JavaScript directly in your render methods, before the return statement, without inserting it inside of curly braces. This is because it is not yet within the JSX code. When you want to use a variable later in the JSX code inside the return statement, you place the variable name inside curly braces.*/
+
 import React, { Component } from 'react'
 
 export class AdvancedJS extends Component {
-  render() {
-    return (
-      <div>
-        jjj
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInput: '',
+            randomIndex: ''
+        }
+        this.ask = this.ask.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    ask() {
+        if (this.state.userInput) {
+            this.setState({
+                randomIndex: Math.floor(Math.random() * 20),
+                userInput: ''
+            });
+        }
+    }
+    handleChange(event) {
+        this.setState({
+            userInput: event.target.value
+        });
+    }
+    render() {
+        const possibleAnswers = [
+            'It is certain',
+            'It is decidedly so',
+            'Without a doubt',
+            'Yes, definitely',
+            'You may rely on it',
+            'As I see it, yes',
+            'Outlook good',
+            'Yes',
+            'Signs point to yes',
+            'Reply hazy try again',
+            'Ask again later',
+            'Better not tell you now',
+            'Cannot predict now',
+            'Concentrate and ask again',
+            'Don\'t count on it',
+            'My reply is no',
+            'My sources say no',
+            'Most likely',
+            'Outlook not so good',
+            'Very doubtful'
+        ];
+        const answer = possibleAnswers[this.state.randomIndex]
+        console.log('randomIndex', this.state.randomIndex)
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.state.userInput}
+                    onChange={this.handleChange}
+                    style={inputStyle} /><br />
+                <button onClick={this.ask}>
+                    Ask the Magic Eight Ball!
+                </button><br />
+                <h3>Answer:</h3>
+                <p>
+                    {answer}
+                </p>
+            </div>
+        )
+    }
+}
+
+const inputStyle = {
+    width: 235,
+    margin: 5
 }
 
 export default AdvancedJS
