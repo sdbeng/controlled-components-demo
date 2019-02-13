83061415542c41f88bcf06d5ed3567cad1ae411b
Sergio Barrera <sdbeng@gmail.com>
2019-02-12 21:09:08 -0800

    advanced javascript in render method

---
+++ src/components/AdvancedJS.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AdvancedJS extends Component {
+  render() {
+    return (
+      <div>
+        jjj
+      </div>
+    )
+  }
+}
+
+export default AdvancedJS
