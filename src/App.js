c871f9002a3d2a522db9e8168e894874890d40b3
Sergio Barrera <sdbeng@gmail.com>
2019-02-14 15:43:47 -0800

    WIP:ternary conditions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Form from "./components/Form";
 import MyComponent from './components/MyComponent'
 import EventListener from "./components/EventListener";
+import Ternary from "./components/Ternary";
 
 function App() {
   return (
   <div>
   
-  <EventListener />
+  <Ternary />
   </div>)
 
 }
