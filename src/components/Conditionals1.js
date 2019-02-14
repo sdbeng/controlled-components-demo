e81949f3ce4c4eea2f0c4ecc3cad9fc06d58bcd6
Sergio Barrera <sdbeng@gmail.com>
2019-02-14 07:09:28 -0800

    render conditionals basics

---
+++ src/components/Conditionals1.js
@@ -0,0 +1,18 @@
+/* React: Render with an If/Else Condition
+Another application of using JavaScript to control your rendered view is to tie the elements that are rendered to a condition. When the condition is true, one view renders. When it's false, it's a different view. You can do this with a standard if/else statement in the render() method of a React component.
+
+ */
+
+import React, { Component } from 'react'
+
+export class Conditionals1 extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Conditionals1
