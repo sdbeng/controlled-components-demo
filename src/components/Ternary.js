693899967c266e4a118c0a5eb984dfa341e16827
Sergio Barrera <sdbeng@gmail.com>
2019-02-15 15:56:40 -0800

    fix logic

--- src/components/Ternary.js
+++ src/components/Ternary.js
@@ -42,7 +42,8 @@ export class Ternary extends Component {
         value={this.state.input}
         onChange={this.handleChange} /><br />
       
-      {buttonOne}
+        { this.state.input >= 18 ? buttonTwo : (this.state.userAge=='')? buttonOne:buttonThree}
+      
       <h1>userAge: {this.state.userAge}</h1>
       </div>
     )
