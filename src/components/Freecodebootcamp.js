8ea994914ea439140c5b5ee798909602a5b92f23
Sergio Barrera <sdbeng@gmail.com>
2019-03-20 07:57:57 -0700

    create Freecodebootcamp component

---
+++ src/components/Freecodebootcamp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Freecodebootcamp extends Component {
+  render() {
+    return (
+      <div>
+        Hello Freecodebootcamp!
+      </div>
+    )
+  }
+}
+
+export default Freecodebootcamp
